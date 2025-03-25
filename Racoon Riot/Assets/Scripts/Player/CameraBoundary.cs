aecfa854a5116dd06b80ca38633ce0505cbefdec
Resipate <105903976+Resipate@users.noreply.github.com>
2025-03-26 03:31:01 +1100

    added camera bounds

--- Racoon Riot/Assets/Scripts/Player/CameraBoundary.cs
+++ Racoon Riot/Assets/Scripts/Player/CameraBoundary.cs
@@ -6,6 +6,17 @@ public class CameraBoundary : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        
+        RaycastHit hit;
+        if(Physics.Linecast(transform.position, _locRef.transform.position, out hit)){
+            _camera.transform.position = new Vector3(
+                Mathf.Lerp(transform.position.x, hit.point.x, 0.9f),
+                Mathf.Lerp(transform.position.y, hit.point.y, 0.9f),
+                Mathf.Lerp(transform.position.z, hit.point.z, 0.9f)
+            );
+        }
+        else{
+            _camera.transform.position = _locRef.transform.position;
+        }
+        _camera.transform.LookAt(_rotRef.transform.position);
     }
 }
