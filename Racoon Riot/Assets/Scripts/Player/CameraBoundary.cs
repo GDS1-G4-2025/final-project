926425c42499b68252ec036e2b9e124ad0a4b012
Resipate <105903976+Resipate@users.noreply.github.com>
2025-03-26 03:13:01 +1100

    added nest and script shell

---
+++ Racoon Riot/Assets/Scripts/Player/CameraBoundary.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraBoundary : MonoBehaviour
+{
+    [SerializeField] private GameObject _camera, _locRef, _rotRef;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+    {
+        
+    }
+}
