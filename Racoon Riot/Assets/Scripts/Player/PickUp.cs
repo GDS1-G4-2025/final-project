ca86186b6bd756d10447d3cf676b4b184b566252
RRahardjo88 <raissarahardjo@gmail.com>
2025-03-30 01:33:23 +1100

    input binding fix

--- Racoon Riot/Assets/Scripts/Player/PickUp.cs
+++ Racoon Riot/Assets/Scripts/Player/PickUp.cs
@@ -1,23 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickUp : MonoBehaviour
 {
     public GameObject Object;
     public GameObject racoonHand;
+    private InputAction _interactAction;
+    private PlayerInput _playerInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
+        _interactAction = _playerInput.actions.FindAction("Interact");
+        if (_interactAction != null)
+        {
+            _interactAction.performed += ctx => PickUpObject();
+        }
+    }
+
+    void OnDestroy ()
+    {
+        if (_interactAction != null)
+        {
+            _interactAction.performed -= ctx => PickUpObject();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.E) && Object != null)
-        {
-            PickUpObject();
-        }
         if (Object != null && Object.transform.parent == racoonHand.transform)
         {
             Object.transform.position = racoonHand.transform.position;
