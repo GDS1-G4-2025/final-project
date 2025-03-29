0ac3f6e4c5c6387ce646e3ae2d017d78e6e08c51
Resipate <105903976+Resipate@users.noreply.github.com>
2025-03-30 02:35:55 +1100

    Resolved Pickup Input Issues

--- Racoon Riot/Assets/Scripts/Player/PickUp.cs
+++ Racoon Riot/Assets/Scripts/Player/PickUp.cs
@@ -3,8 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PickUp : MonoBehaviour
 {
-    public GameObject Object;
-    public GameObject racoonHand;
+    [SerializeField] private GameObject _pickUpTarget;
+    [SerializeField] private GameObject _heldItemHandler;
     private InputAction _interactAction;
     private PlayerInput _playerInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,50 +12,49 @@ public class PickUp : MonoBehaviour
     {
         _playerInput = GetComponent<PlayerInput>();
         _interactAction = _playerInput.actions.FindAction("Interact");
-        if (_interactAction != null)
-        {
-            _interactAction.performed += ctx => PickUpObject();
-        }
     }
 
-    void OnDestroy ()
+    void OnEnable()
     {
-        if (_interactAction != null)
-        {
-            _interactAction.performed -= ctx => PickUpObject();
-        }
+        _interactAction.performed += PickUpObject;
+    }
+
+    void OnDisable()
+    {
+        _interactAction.performed -= PickUpObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Object != null && Object.transform.parent == racoonHand.transform)
+        if (_pickUpTarget != null && _pickUpTarget.transform.parent == _heldItemHandler.transform)
         {
-            Object.transform.position = racoonHand.transform.position;
+            _pickUpTarget.transform.position = _heldItemHandler.transform.position;
         }
         
     }
-    public void PickUpObject(){
-        Object.transform.SetParent(racoonHand.transform);
-        Object.transform.localPosition = Vector3.zero;
-
-        
+    public void PickUpObject(InputAction.CallbackContext ctx){
+        Debug.Log("picking up");
+        if(_pickUpTarget != null){
+            _pickUpTarget.transform.SetParent(_heldItemHandler.transform);
+            _pickUpTarget.transform.localPosition = Vector3.zero;
+        }
     }
     public void OnTriggerEnter(Collider other)
     {
-        if(Object == null && other.CompareTag("PickUpObject"))
+        if(_pickUpTarget == null && other.CompareTag("PickUpObject"))
         {
-            Object = other.gameObject;
+            _pickUpTarget = other.gameObject;
             Debug.Log (other.gameObject.name + " is picked up!");
 
         }
     }
     public void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("PickUpObject") && Object == other.gameObject)
+        if(other.CompareTag("PickUpObject") && _pickUpTarget == other.gameObject)
         {
-            Object = null;
+            _pickUpTarget = null;
 
         }
     }
