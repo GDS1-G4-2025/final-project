494a10d6ca897b804ecf8dfcc23c8d3669a4b892
RRahardjo88 <raissarahardjo@gmail.com>
2025-03-30 12:32:39 +1100

    throwing input and logic. needs #62 to work.

--- Racoon Riot/Assets/Scripts/Player/PickUp.cs
+++ Racoon Riot/Assets/Scripts/Player/PickUp.cs
@@ -5,58 +5,83 @@ public class PickUp : MonoBehaviour
 {
     public GameObject Object;
     public GameObject racoonHand;
+    public float throwForce = 10f;
+    [SerializeField] private GameObject _pickUpTarget;
+    [SerializeField] private GameObject _heldItemHandler;
     private InputAction _interactAction;
     private PlayerInput _playerInput;
+    private InputAction _throwAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _interactAction = _playerInput.actions.FindAction("Interact");
-        if (_interactAction != null)
-        {
-            _interactAction.performed += ctx => PickUpObject();
-        }
+        _throwAction = _playerInput.actions.FindAction("Throw");
     }
 
-    void OnDestroy ()
+    void OnEnable()
     {
-        if (_interactAction != null)
-        {
-            _interactAction.performed -= ctx => PickUpObject();
-        }
+        _interactAction.performed += PickUpObject;
+        _throwAction.performed += ThrowObject;
+    }
+
+    void OnDisable()
+    {
+        _interactAction.performed -= PickUpObject;
+        _throwAction.performed -= ThrowObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Object != null && Object.transform.parent == racoonHand.transform)
+        if (_pickUpTarget != null && _pickUpTarget.transform.parent == _heldItemHandler.transform)
         {
-            Object.transform.position = racoonHand.transform.position;
+            _pickUpTarget.transform.position = _heldItemHandler.transform.position;
         }
         
     }
-    public void PickUpObject(){
-        Object.transform.SetParent(racoonHand.transform);
-        Object.transform.localPosition = Vector3.zero;
-
-        
+    public void PickUpObject(InputAction.CallbackContext ctx){
+        Debug.Log("picking up");
+        if(_pickUpTarget != null){
+            _pickUpTarget.transform.SetParent(_heldItemHandler.transform);
+            _pickUpTarget.transform.localPosition = Vector3.zero;
+        }
+    }
+    public void ThrowObject(InputAction.CallbackContext ctx)
+    {
+        if (_pickUpTarget != null)
+        {
+            ThrowableItem throwable = _pickUpTarget.GetComponent<ThrowableItem>();
+            if (throwable != null)
+            {
+                Vector3 throwDirection = transform.forward;
+                throwable.Throw(throwDirection);
+                _pickUpTarget = null;
+            }
+        }
     }
     public void OnTriggerEnter(Collider other)
     {
-        if(Object == null && other.CompareTag("PickUpObject"))
+        if(_pickUpTarget == null && other.CompareTag("PickUpObject"))
         {
-            Object = other.gameObject;
+            _pickUpTarget = other.gameObject;
             Debug.Log (other.gameObject.name + " is picked up!");
 
         }
     }
     public void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("PickUpObject") && Object == other.gameObject)
+        if(other.CompareTag("PickUpObject") && _pickUpTarget == other.gameObject)
         {
-            Object = null;
+            _pickUpTarget = null;
 
         }
     }
 }
+
+
+
+
+
+
